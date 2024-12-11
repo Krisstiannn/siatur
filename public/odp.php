44e7670026cae0296e3c9a46421a1fbcbd863a2d
Kris263 <kristianptr2633@gmail.com>
2024-12-11 22:47:19 +0800

    Pembuatan Form

---
+++ public/odp.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Barang Masuk</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Preloader -->
+        <?php include "../layouts/loading.php"?>
+
+        <!-- Navbar -->
+        <?php include "../layouts/header.php"?>
+        <!-- Navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php include "../layouts/sidebar.php"?>
+        <!-- END Main Sidebar -->
+
+        <!-- Main Content -->
+        <div class="content-wrapper">
+
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Data Pemasangan ODP</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- Left col -->
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-6">
+                                </div>
+                                <div class="col-md-6">
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header border-transparent">
+                                    <h3 class="card-title">Latest Orders</h3>
+
+                                    <div class="card-tools">
+                                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                            <i class="fas fa-minus"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                            <i class="fas fa-times"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="card-body p-0">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>Redaman IN ODP</th>
+                                                    <th>Redaman IN PSB</th>
+                                                    <th>Redaman OUT Selanjutnya</th>
+                                                    <th>Redaman Pelanggan</th>
+                                                    <th>Jenis SPL Rasio</th>
+                                                    <th>Titik Kordinat Pemasangan ODP</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                                                    <td>Call of Duty IV</td>
+                                                    <td><span class="badge badge-success">Shipped</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR1848</a></td>
+                                                    <td>Samsung Smart TV</td>
+                                                    <td><span class="badge badge-warning">Pending</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#f39c12" data-height="20">
+                                                            90,80,-90,70,61,-83,68</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                                                    <td>iPhone 6 Plus</td>
+                                                    <td><span class="badge badge-danger">Delivered</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#f56954" data-height="20">
+                                                            90,-80,90,70,-61,83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                                                    <td>Samsung Smart TV</td>
+                                                    <td><span class="badge badge-info">Processing</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00c0ef" data-height="20">
+                                                            90,80,-90,70,-61,83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR1848</a></td>
+                                                    <td>Samsung Smart TV</td>
+                                                    <td><span class="badge badge-warning">Pending</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#f39c12" data-height="20">
+                                                            90,80,-90,70,61,-83,68</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                                                    <td>iPhone 6 Plus</td>
+                                                    <td><span class="badge badge-danger">Delivered</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#f56954" data-height="20">
+                                                            90,-80,90,70,-61,83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                                                    <td>Call of Duty IV</td>
+                                                    <td><span class="badge badge-success">Shipped</span></td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="sparkbar" data-color="#00a65a" data-height="20">
+                                                            90,80,90,-70,61,-83,63</div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-info btn-xs">Edit</button>
+                                                        <button type="button"
+                                                            class="btn btn-block btn-danger btn-xs">Hapus</button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="card-footer clearfix">
+                                    <a href="javascript:void(0)" class="btn btn-sm btn-success float-right ">Tambah
+                                        Data</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- END Main Content -->
+
+        <!-- Main Footer -->
+        <?php include "../layouts/footer.php"?>
+        <!-- End Footer -->
+    </div>
+
+    <script src="../plugins/jquery/jquery.min.js"></script>
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="../dist/js/adminlte.js"></script>
+    <script src="../plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+    <script src="../plugins/raphael/raphael.min.js"></script>
+    <script src="../plugins/jquery-mapael/jquery.mapael.min.js"></script>
+    <script src="../plugins/jquery-mapael/maps/usa_states.min.js"></script>
+    <script src="../plugins/chart.js/Chart.min.js"></script>
+    <script src="../dist/js/demo.js"></script>
+    <script src="../dist/js/pages/dashboard2.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
---
+++ public/tambah-keluar.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Barang Keluar</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Preloader -->
+        <?php include "../layouts/loading.php"?>
+
+        <!-- Navbar -->
+        <?php include "../layouts/header.php"?>
+        <!-- Navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php include "../layouts/sidebar.php"?>
+        <!-- END Main Sidebar -->
+
+        <!-- Main Content -->
+        <div class="content-wrapper">
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Barang Keluar</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="content">
+                <div class="content-fluid">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Input Barang</h3>
+                        </div>
+                        <form>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Kode Barang</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1"
+                                        placeholder="Kode Barang">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Nama Barang</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                        placeholder="Masukkan Nama Barang">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Penggunaan Barang</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1"
+                                        placeholder="Penggunaan Barang">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Jumlah</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1"
+                                        placeholder="Jumlah">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">Gambar Barang</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- END Main Content -->
+
+        <!-- Main Footer -->
+        <?php include "../layouts/footer.php"?>
+        <!-- End Footer -->
+    </div>
+
+    <script src="../plugins/jquery/jquery.min.js"></script>
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="../dist/js/adminlte.js"></script>
+    <script src="../plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+    <script src="../plugins/raphael/raphael.min.js"></script>
+    <script src="../plugins/jquery-mapael/jquery.mapael.min.js"></script>
+    <script src="../plugins/jquery-mapael/maps/usa_states.min.js"></script>
+    <script src="../plugins/chart.js/Chart.min.js"></script>
+    <script src="../dist/js/demo.js"></script>
+    <script src="../dist/js/pages/dashboard2.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
